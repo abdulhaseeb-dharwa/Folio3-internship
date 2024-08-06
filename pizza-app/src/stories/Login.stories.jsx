6278ba49d3b3f018c1313b0379668430af7446ad
Abdul Haseeb Dharwarwala <haseebfaisal2002@gmail.com>
2024-08-06 16:16:19 +0500

    added story book

---
+++ pizza-app/src/stories/Login.stories.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from '../store/Auth-slice';
+import Login from '../pages/Login';
+
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+  },
+});
+
+export default {
+  title: 'Components/Login',
+  component: Login,
+  decorators: [
+    (Story) => (
+      <Provider store={store}>
+        <BrowserRouter>
+          <Story />
+        </BrowserRouter>
+      </Provider>
+    ),
+  ],
+};
+
+const Template = (args) => <Login {...args} />;
+
+export const DefaultLogin = Template.bind({});
+DefaultLogin.args = {};
